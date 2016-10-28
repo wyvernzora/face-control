e5a5b528287576be505666a36fe6c1c1cfe37a80
Denis Luchkin-Zhou <denis@ricepo.com>
2016-10-27 17:36:32 -0700

    feature: Exposing new$ as option to construct new instance

--- src/index.js
+++ src/index.js
@@ -9,63 +9,75 @@ import Check       from './check/request';
 import Collate     from './util/collate';
 import Manager     from './manager';
 
-/*!
- * Permission manager.
+
+/**
+ * Factory function
  */
-const manager = new Manager();
+function new$() {
+
+  /*!
+   * Permission manager.
+   */
+  const manager = new Manager();
 
 
-/*!
- * Function to retroactively check additional permissions.
- */
-function can(req, ...actions) {
+  /*!
+   * Function to retroactively check additional permissions.
+   */
+  function can(req, ...actions) {
 
-  /* Get the scope-role tree */
-  const tree = Collate(manager, actions);
+    /* Get the scope-role tree */
+    const tree = Collate(manager, actions);
 
-  /* Check the permissions */
-  return Check(manager, tree, req);
+    /* Check the permissions */
+    return Check(manager, tree, req);
 
+  }
+
+
+  /*!
+   * Middleware factory.
+   */
+  function FaceControl(...actions) {
+
+    /* Get the scope-role tree */
+    const tree = Collate(manager, actions);
+
+    /* Create the middleware function that checks the request */
+    return function(req, res, next) {
+      const promise = Check(manager, tree, req);
+
+      promise
+        .then(result => {
+          if (!result) {
+            const error = new Error(`Permission denied: ${actions.join(', ')}`);
+            error.name = 'AuthorizationError';
+            throw error;
+          }
+          next();
+        })
+        .catch(next);
+    };
+
+  }
+
+
+  /*!
+   * Expose manager functions via FaceControl export.
+   */
+  FaceControl.scope  = manager.scope.bind(manager);
+  FaceControl.role   = manager.role.bind(manager);
+  FaceControl.action = manager.action.bind(manager);
+  FaceControl.imply  = manager.imply.bind(manager);
+  FaceControl.can    = can;
+
+
+  return FaceControl;
 }
 
-
-/*!
- * Middleware factory.
- */
-function FaceControl(...actions) {
-
-  /* Get the scope-role tree */
-  const tree = Collate(manager, actions);
-
-  /* Create the middleware function that checks the request */
-  return function(req, res, next) {
-    const promise = Check(manager, tree, req);
-
-    promise
-      .then(result => {
-        if (!result) {
-          const error = new Error(`Permission denied: ${actions.join(', ')}`);
-          error.name = 'AuthorizationError';
-          throw error;
-        }
-        next();
-      })
-      .catch(next);
-  };
-
-}
-
-
-/*!
- * Expose manager functions via FaceControl export.
- */
-FaceControl.scope  = manager.scope.bind(manager);
-FaceControl.role   = manager.role.bind(manager);
-FaceControl.action = manager.action.bind(manager);
-FaceControl.imply  = manager.imply.bind(manager);
-FaceControl.can    = can;
 
 /*!
  * Export the thing.
  */
-module.exports = FaceControl;
+module.exports = new$();
+module.exports.new$ = new$;
